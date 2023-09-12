2436bdc27b8a3a8fb7589ada1dc3019169028aa6
“Imraan” <imraanmohamedismail@gmail.com>
2023-09-13 04:19:09 +0530

    React Day-2 task

---
+++ React_Task/Day-2/price-tag/src/Landing.jsx
@@ -0,0 +1,278 @@
+import React, { Component } from "react";
+
+export class Landing extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      { class: "wrapper" },
+      React.createElement(
+        "div",
+        { class: "table basic" },
+        React.createElement(
+          "div",
+          { class: "price-section" },
+          React.createElement(
+            "div",
+            { class: "price-area" },
+            React.createElement(
+              "div",
+              { class: "inner-area" },
+              React.createElement(
+                "span",
+                { class: "text" },
+                React.createElement("span", null, "$"),
+                React.createElement("span", "Price", "00")
+              )
+            )
+          )
+        ),
+        React.createElement("div", { class: "package-name" }),
+        React.createElement(
+          "div",
+          { class: "features" },
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "One Selected Template"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "100% Responsive Design"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Credit Remove Permission"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-times-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Lifetime Template Updates"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-times-circle" })
+            )
+          ),
+          React.createElement(
+            "div",
+            { class: "btn" },
+            React.createElement("button", null, "Purchase")
+          )
+        )
+      ),
+
+      React.createElement(
+        "div",
+        { class: "table premium" },
+        React.createElement(
+          "div",
+          { class: "price-section" },
+          React.createElement(
+            "div",
+            { class: "price-area" },
+            React.createElement(
+              "div",
+              { class: "inner-area" },
+              React.createElement(
+                "span",
+                { class: "text" },
+                React.createElement("span", null, "$"),
+                React.createElement("span", "Price", "49")
+              )
+            )
+          )
+        ),
+        React.createElement("div", { class: "package-name" }),
+        React.createElement(
+          "div",
+          { class: "features" },
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Five Existing Template"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "100% Responsive Design"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Credit Remove Permission"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Lifetime Template Updates"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-times-circle" })
+            )
+          ),
+          React.createElement(
+            "div",
+            { class: "btn" },
+            React.createElement("button", null, "Purchase")
+          )
+        )
+      ),
+
+      React.createElement(
+        "div",
+        { class: "table Ultimate" },
+        React.createElement(
+          "div",
+          { class: "price-section" },
+          React.createElement(
+            "div",
+            { class: "price-area" },
+            React.createElement(
+              "div",
+              { class: "inner-area" },
+              React.createElement(
+                "span",
+                { class: "text" },
+                React.createElement("span", null, "$"),
+                React.createElement("span", "Price", "99")
+              )
+            )
+          )
+        ),
+        React.createElement("div", { class: "package-name" }),
+        React.createElement(
+          "div",
+          { class: "features" },
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "All Existing Template"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "100% Responsive Design"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Credit Remove Permission"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "li",
+            {},
+            React.createElement(
+              "span",
+              { class: "list-name" },
+              "Lifetime Template Updates"
+            ),
+            React.createElement(
+              "span",
+              { class: "icon check" },
+              React.createElement("i", { class: "fas fa-check-circle" })
+            )
+          ),
+          React.createElement(
+            "div",
+            { class: "btn" },
+            React.createElement("button", null, "Purchase")
+          )
+        )
+      )
+    );
+  }
+}
+
+export default Landing;
